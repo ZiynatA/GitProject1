b43e4532974eda6ff72622c59eda99d788c5bb3c
Ziynat <azyinata107@gmail.com>
2024-02-01 18:26:32 +0300

    Added a method to Iphone 14

--- src/apple/iphone14.java
+++ src/apple/iphone14.java
@@ -4,4 +4,7 @@ public class iphone14 {
     public static void main(String[] args) {
         System.out.println("What a phone!");
     }
+    public static void calling(){
+        System.out.println("Tuuut tuut");
+    }
 }
