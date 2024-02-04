0131ecf6bd09bc7493238d57377eda862f181a77
Ziynat <azyinata107@gmail.com>
2024-02-04 22:40:38 +0300

    Added/Method to the first level

--- src/levels/A.java
+++ src/levels/A.java
@@ -4,4 +4,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("First level");
     }
+    public static void defineTheLevel(){
+        System.out.println("Have an exam");
+    }
 }
