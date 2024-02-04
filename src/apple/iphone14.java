7e9e70cb98ac0ddbd5983f90e184560dbaf42c4c
Ziynat <azyinata107@gmail.com>
2024-02-04 20:41:19 +0300

    Added/First level

--- src/apple/iphone14.java
+++ src/apple/iphone14.java
@@ -4,7 +4,4 @@ public class iphone14 {
     public static void main(String[] args) {
         System.out.println("What a phone!");
     }
-    public static void calling(){
-        System.out.println("Tuuut tuut");
-    }
 }
---
+++ src/levels/A.java
@@ -0,0 +1,7 @@
+package levels;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("First level");
+    }
+}
