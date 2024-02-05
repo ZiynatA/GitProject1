356c7f71c0a75dea642bb7a38fc44b4a16f960b9
Ziynat <azyinata107@gmail.com>
2024-02-05 12:39:51 +0300

    Added message into class B

--- src/levels/A.java
+++ src/levels/A.java
@@ -4,7 +4,4 @@ public class A {
     public static void main(String[] args) {
         System.out.println("First level");
     }
-    public static void defineTheLevel(){
-        System.out.println("Have an exam");
-    }
 }
--- src/levels/B.java
+++ src/levels/B.java
@@ -4,4 +4,8 @@ public class B {
     public static void main(String[] args) {
         System.out.println("The second level");
     }
+
+    public static void message(){
+        System.out.println("Write your message here");
+    }
 }
